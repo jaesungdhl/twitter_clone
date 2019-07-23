dcf64e88c18d7abae8140f8f3349bbe7f21ad164
Jaesung Lee <jaesungdhl@gmail.com>
2019-07-23 16:59:34 -0700

    Finish Main Page Profile Card

--- src/components/MainPage/ProfileCard.jsx
+++ src/components/MainPage/ProfileCard.jsx
@@ -10,9 +10,26 @@ const ProfileCard = (props) => {
       </div>
       <div class="card-body">
         <div className="profile-pic-user-name-container">
-          <div class="profile-pic-container"><i class="fas fa-plus-circle"></i></div>
+          <div class="profile-pic-container"><div class="profile-pic-container-border"><i class="fas fa-plus-circle"></i></div></div>
+          <div className="profile-user-info-container">
+            <a className="user-name" href="#">John Lee</a>
+            <a className="user-username" href="#">@JohnLee119</a>
+          </div>
         </div>
-        <p class="card-text"></p>
+        <div className="twitter-data-info">
+          <a className="twitter-data-list" href="#">
+            <span className="list-title">Tweets</span>
+            <span className="list-counter" id="tweet-count">1</span>
+          </a>
+          <a className="twitter-data-list" href="#">
+            <span className="list-title">Followers</span>
+            <span className="list-counter" id="follower-count">10</span>
+          </a>
+          <a className="twitter-data-list" href="#">
+            <span className="list-title">Following</span>
+            <span className="list-counter" id="following-count">100</span>
+          </a>
+        </div>
       </div>
     </div>
   );
